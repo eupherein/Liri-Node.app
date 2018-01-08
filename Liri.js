4a8b2f611ba94ba1e2db19f00acf0b88a50e68d1
Derek Bocskey <bocskey.derek@gmail.com>
2018-01-08 03:07:21 -0500

    got ombd to sort of work

--- Liri.js
+++ Liri.js
@@ -9,13 +9,32 @@ var twitter = require('twitter');
 var spotify = require('node-spotify-api');
 var request = require('request');
 
-//argv stuff
+//variables for holding api search parameters
 var userInput = process.argv[2];
+var movieInput = process.argv[3];
+var songInput = "";
+var movieName = "";
 
-var searchInput = "";
-
+//loops for using more than one word searches
+//spotify
 for (i = 3; i < process.argv.length; i++)
-    searchInput += process.argv[i] + "+"
+ songInput += process.argv[i] + "+"
+//ombd
+for (var i = 2; i < userInput.length; i++) {
+
+  if (i > 2 && i < userInput.length) {
+
+    movieName = movieName + "+" + userInput[i];
+
+  }
+
+  else {
+
+    movieName += userInput[i];
+
+  }
+}
+
 
     //code required to import key.js
 var spotify = new spotify(keys.spotify);
@@ -37,7 +56,7 @@ function showTweets() {
 //function for searching spotify song 
 function searchSong() {
     console.log("result: ")
-    spotify.search({ type: 'track', query: searchInput}, function(err, data) {
+    spotify.search({ type: 'track', query: songInput}, function(err, data) {
         if ( err ) {
             console.log('Error occurred: ' + err);
             return;
@@ -54,6 +73,27 @@ function searchSong() {
     });
 }
 
+//function for searching omdb movie
+function searchMovie() {
+    console.log("result: ")
+    //api call for movie title search
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+// for error
+console.log(queryUrl);
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+    // Parse the body of the site and recover just the imdbRating
+    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+    console.log("Release Year: " + JSON.parse(body).Year);
+  }
+});
+}
+
 //switch method
 switch(userInput) {
     case 'my-tweets':
@@ -66,7 +106,7 @@ switch(userInput) {
         break;
     case 'movie-this':
         console.log("\n ombd fired! \n");
-        //searchOmbd();
+        searchMovie();
     case 'do-what-it-says':
         console.log("\n doThis fired! \n");
-}
+}  
