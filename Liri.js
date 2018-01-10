1758a20de1edb0ac434bf84f9364924739f8290f
Derek Bocskey <bocskey.derek@gmail.com>
2018-01-09 22:44:44 -0500

    got spotify working

--- Liri.js
+++ Liri.js
@@ -9,41 +9,15 @@ var twitter = require('twitter');
 var spotify = require('node-spotify-api');
 var request = require('request');
 
-//variables for holding api search parameters
-var userInput = process.argv[2];
-var movieInput = process.argv[3];
-var songInput = "";
-var movieName = "";
 
-//loops for using more than one word searches
-//spotify
-for (i = 3; i < process.argv.length; i++)
- songInput += process.argv[i] + "+"
-//ombd
-for (var i = 2; i < userInput.length; i++) {
-
-  if (i > 2 && i < userInput.length) {
-
-    movieName = movieName + "+" + userInput[i];
-
-  }
-
-  else {
-
-    movieName += userInput[i];
-
-  }
-}
-
-
-    //code required to import key.js
+//code required to import key.js
 var spotify = new spotify(keys.spotify);
 var client = new twitter(keys.twitter);
 
 //function for pulling 20 tweets from twitter api
 function showTweets() {
     console.log("Here are your tweets: ")
-    var params = { screen_name: 'eusEJ1', count: 5};
+    var params = { screen_name: 'eusEJ1', count: 5 };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             for (i = 0; i < tweets.length; i++)
@@ -52,61 +26,87 @@ function showTweets() {
     });
 }
 
+//ALL FUNCTIONS----------------------------
+// pulls artist name
+var artistName = function (artist) {
+    return artist.name;
+};
 
-//function for searching spotify song 
-function searchSong() {
-    console.log("result: ")
-    spotify.search({ type: 'track', query: songInput}, function(err, data) {
-        if ( err ) {
-            console.log('Error occurred: ' + err);
-            return;
+var songSearch = function (songInput) {
+    if (songInput === undefined) {
+        songInput = "not found!"
+    }
+
+    // function for searching for song
+    spotify.search(
+        {
+            type: 'track',
+            query: songInput
+        },
+        function (err, data) {
+            if (err) {
+                console.log('Error occurred: ' + err);
+                return;
+            }
+
+            // Do something with 'data'   
+            var songInfo = data.tracks.items;
+
+
+            for (var i = 0; i < songs.length; i++) {
+
+                // console.log(songInfo);
+                console.log(i);
+                console.log("Artist:" + songInfo[i].album.artists[0].name);
+                console.log("Song:" + songInfo[i].name);
+                console.log("Preview link:" + songInfo[i].preview_url);
+                console.log("Album:" + songInfo[i].album.name);
+            }
         }
-     
-        // Do something with 'data'   
-        var songInfo = response.tracks.items[0];
-  
-        // console.log(songInfo);
-  console.log("Artist:", songInfo.album.artists[0].name);
-  console.log("Song:", songInfo.name);
-  console.log("Preview link:", songInfo.preview_url);
-  console.log("Album:", songInfo.album.name);
+    );
+};
+
+//function for searching omdb movie
+var searchMovie = function () {
+    console.log("result: ")
+    //api call for movie title search
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    // for error
+    console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("Release Year: " + JSON.parse(body).Year);
+        }
     });
 }
 
-//function for searching omdb movie
-function searchMovie() {
-    console.log("result: ")
-    //api call for movie title search
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-// for error
-console.log(queryUrl);
-
-request(queryUrl, function(error, response, body) {
-
-  // If the request is successful
-  if (!error && response.statusCode === 200) {
-
-    // Parse the body of the site and recover just the imdbRating
-    // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    console.log("Release Year: " + JSON.parse(body).Year);
-  }
-});
-}
-
 //switch method
-switch(userInput) {
-    case 'my-tweets':
-        console.log("\n twitter fired! \n");
-        showTweets();
-        break;
-    case 'spotify':
-        console.log("\n spotify fired! \n");
-        searchSong();
-        break;
-    case 'movie-this':
-        console.log("\n ombd fired! \n");
-        searchMovie();
-    case 'do-what-it-says':
-        console.log("\n doThis fired! \n");
-}  
+var choose = function (caseData, functionData) {
+    switch (userInput) {
+        case 'tweets':
+            console.log("\n twitter fired! \n");
+            showTweets();
+            break;
+        case 'spotify':
+            console.log("\n spotify fired! \n");
+            searchSong();
+            break;
+        case 'movie':
+            console.log("\n ombd fired! \n");
+            searchMovie();
+        case 'do':
+            console.log("\n doThis fired! \n");
+    }
+};
+
+//function for running the function being called on in
+
+//runs the stuff
+runStuff(process.argv[2], process.argv[3])
